d192cabca43b80fdc1a134da3fe1ab405ff8f343
sanaullah129 <109277244+sanaullah129@users.noreply.github.com>
2023-07-20 23:44:16 +0530

    Schema setup completed

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const connectToMongo = require('./db');
+const express = require('express');
 
-const express = require('express')
+
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
